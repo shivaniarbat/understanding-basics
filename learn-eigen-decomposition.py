35f454c82e9daa1019744c4787f8ad10ee9e20ea
Shivani Arbat <shivani.arbat@gmail.com>
2019-08-27 16:06:11 -0400

    basics for eigendecomposition

---
+++ learn-eigen-decomposition.py
@@ -0,0 +1,31 @@
+import numpy as np
+from numpy import dot
+
+# define an array
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(A)
+
+# calculate eigendecomposition
+values,vectors = np.linalg.eig(A)
+
+# A.v = λ.v
+# A is square matrix 
+# v is of size of A
+# λ (eigen values) is usually a list 
+# so for the given example the 
+#           first eigenvector is vectors[:, 0] and eigenvalues is values[0]
+#           second eigenvector is vectors[:, 1] and eigenvalues is values[2]
+#           third eigenvector is vectors[:, 2] and eigenvalues is values[2]
+
+# verify eigendecomposition 
+# first 
+print(A.dot(vectors[:,0]))
+print(vectors[:,0] * values[0])
+
+# second 
+print(A.dot(vectors[:,1]))
+print(vectors[:,1] * values[1])
+
+# third 
+print(A.dot(vectors[:,2]))
+print(vectors[:,2] * values[2])
